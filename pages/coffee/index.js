86848ce7b719a7623da75126f36931c781d9366f
cerdemrc <cerdemrc@gmail.com>
2022-01-13 20:46:07 +0300

    eslint

--- pages/coffee/index.js
+++ pages/coffee/index.js
@@ -1,32 +1,33 @@
-import Head from "next/head"
-import styles from '../../public/styles/coffee.module.scss'
+import Head from 'next/head';
+import styles from '../../public/styles/coffee.module.scss';
 
 export default function coffee({ coffeeList }) {
-    console.log(coffeeList)
-    return (
-        <div className="container">
-            <Head>
-                <title>Coffee</title>
-            </Head>
+	return (
+		<div className='container'>
+			<Head>
+				<title>Coffee</title>
+			</Head>
 
-            <div className={styles.coffee}>
-                {
-                    coffeeList.map(coffee => {
-                        return <a href={`/coffee/${coffee.id}`} key={coffee.id}>{coffee.title}</a>
-                    })
-                }
-            </div>
-        </div>
-    )
+			<div className={styles.coffee}>
+				{coffeeList.map((coffee) => {
+					return (
+						<a href={`/coffee/${coffee.id}`} key={coffee.id}>
+							{coffee.title}
+						</a>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export async function getStaticProps() {
-    const req = await fetch('https://api.sampleapis.com/coffee/hot');
-    const res = await req.json()
+	const req = await fetch('https://api.sampleapis.com/coffee/hot');
+	const res = await req.json();
 
-    return {
-        props: {
-            coffeeList: res
-        }
-    }
+	return {
+		props: {
+			coffeeList: res,
+		},
+	};
 }
